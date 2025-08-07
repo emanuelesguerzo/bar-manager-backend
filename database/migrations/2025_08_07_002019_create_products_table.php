86d3874dce72da42fac82dc4b080fccd52d0fb5e
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-08-07 04:04:38 +0200

    feat: start work on bar manager 2.0 (Laravel 12, Tailwind CSS and new functionalities)

---
+++ database/migrations/2025_08_07_002019_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("name");
+            $table->string('slug')->unique();
+            $table->string("brand")->nullable();
+            $table->decimal("price", 6, 2);
+            $table->bigInteger("units_in_stock");
+            $table->bigInteger("stock_ml")->nullable();
+            $table->bigInteger("stock_g")->nullable();
+            $table->bigInteger("unit_size_ml")->nullable();
+            $table->bigInteger("unit_size_g")->nullable();
+            $table->string("image")->nullable();
+            $table->foreignId("supplier_id")->nullable()->constrained()->onDelete("set null");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
